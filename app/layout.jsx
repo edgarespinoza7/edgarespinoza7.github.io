923fe780b345535fbfe60fa44dccd678345392c6
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-22 23:49:34 +0100

    13 commit -added Logo and style to the Navbar- OK

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 //Components
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-//Theme Provider
+//Theme Providerj
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 
