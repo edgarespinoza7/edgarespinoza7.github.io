140f6065753dfef5d15789ad70c4e3a1a93de850
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-22 19:39:51 +0100

    11th commit -added pages and paths to pages- OK

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,10 @@
+//Link (next js)
 import Link from "next/link";
+
+//Next Hooks
+import { usePathname } from "next/navigation";
+
+
 
 const links = [
     { path: '/', name: 'home' },
@@ -7,10 +13,23 @@ const links = [
 ];
 
 const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+
+    const path = usePathname();
+
     return (
         <nav className={`${containerStyles}`}>
-            {links.map( (link, index) => {
-                return <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>{link.name}</Link>
+            {links.map((link, index) => {
+                return <Link
+                    href={link.path}
+                    key={index}
+                    className={`capitalize ${linkStyles}`}>
+                    {/* {link.path === path && (
+                      
+
+                        
+                    )} */}
+                    {link.name}
+                </Link>
             })}
         </nav>
     );
