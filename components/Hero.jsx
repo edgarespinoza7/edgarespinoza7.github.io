1d6509351a8bcf613184cfeb5960404234541c88
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-24 18:25:40 +0100

    18 commit -added social icons and style- OK

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -41,7 +41,9 @@ const Hero = () => {
                 <Download size={18} />
               </Button>
             </div>
-            <Socials />
+            <Socials 
+            containerStyles='flex gap-x-6 mx-auto xl:mx-0' 
+            iconsStyles='text-foreground text-[22px] hover:text-primary transition-all' />
           </div>
           <div className="hidden xl:flex relative">image</div>
         </div>
