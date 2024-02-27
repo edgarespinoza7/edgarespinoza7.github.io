1c2d9d10fc21507d9935209b7cea40968ed1502a
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-27 23:58:58 +0100

    22 commit -added About component and Style- OK

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
-import DevImg from "./Devimg";
-import Image from "next/image";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+// import DevImg from "./Devimg";
+// import Image from "next/image";
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 import {
     user2,
@@ -126,20 +126,19 @@ const skillData = [
 
 
 const About = () => {
-    
+
     const getData = (arr, title) => {
-        return arr.find( (item) => item.title === title);
+        return arr.find((item) => item.title === title);
     };
 
-    console.log(getData(qualificationData, 'experience'));
-
     return (
-        <section>
-            <div className="container">
+        <section className="xl:h-[860px] pb-12 xl:py-24">
+            <div className="container mx-auto">
+                <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">About me</h2>
                 
             </div>
         </section>
     )
 }
 
-export default About
+export default About;
