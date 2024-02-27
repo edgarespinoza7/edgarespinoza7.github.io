b52c60eb87b3422b5a17649e74c9d4c912930d7f
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-28 00:30:35 +0100

    23 -added tabs names- OK

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
-// import DevImg from "./Devimg";
-// import Image from "next/image";
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import DevImg from "./Devimg";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 import {
     user2,
@@ -12,6 +12,7 @@ import {
     Briefcase,
     User2,
 } from 'lucide-react';
+
 
 const infoData = [
     {
@@ -135,7 +136,30 @@ const About = () => {
         <section className="xl:h-[860px] pb-12 xl:py-24">
             <div className="container mx-auto">
                 <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">About me</h2>
-                
+                <div className="flex flex-col xl:flex-row">
+                    {/* Image */}
+                    <div className="hidden xl:flex flex-1 relative">
+                        <DevImg 
+                        containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative" imgSrc="/about/developer.png"/>
+                    </div>
+                    {/* Tabs */}
+                    <div className="flex-1">
+                        <Tabs defaultValue="personal">
+                            <TabsList>
+                                <TabsTrigger value="personal">Personal Info</TabsTrigger>
+                                <TabsTrigger value="qualifications">Qualifications</TabsTrigger>
+                                <TabsTrigger value="skills">Skills</TabsTrigger>
+                            </TabsList>
+                            {/* Tabs Content */}
+                            <div className="text-lg mt-12 xl:mt-8">
+                            {/* Personal */}
+                                <TabsContent value="personal">Personal Info</TabsContent>
+                                <TabsContent value="qualifications">Qualifications</TabsContent>
+                                <TabsContent value="skills">Skills</TabsContent>
+                            </div>
+                        </Tabs>
+                    </div>
+                </div>
             </div>
         </section>
     )
