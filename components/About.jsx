c8ec469e56aaf9d4d07257e61edc0a4b7d1356f8
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-28 13:25:21 +0100

    24 added tab with personal info and style- OK

--- components/About.jsx
+++ components/About.jsx
@@ -145,17 +145,31 @@ const About = () => {
                     {/* Tabs */}
                     <div className="flex-1">
                         <Tabs defaultValue="personal">
-                            <TabsList>
-                                <TabsTrigger value="personal">Personal Info</TabsTrigger>
-                                <TabsTrigger value="qualifications">Qualifications</TabsTrigger>
-                                <TabsTrigger value="skills">Skills</TabsTrigger>
+                            <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
+                                <TabsTrigger className="w-[162px] xl:auto" value="personal">Personal Info</TabsTrigger>
+                                <TabsTrigger className="w-[162px] xl:auto" value="qualifications">Qualifications</TabsTrigger>
+                                <TabsTrigger className="w-[162px] xl:auto" value="skills">Skills</TabsTrigger>
                             </TabsList>
                             {/* Tabs Content */}
                             <div className="text-lg mt-12 xl:mt-8">
                             {/* Personal */}
-                                <TabsContent value="personal">Personal Info</TabsContent>
-                                <TabsContent value="qualifications">Qualifications</TabsContent>
-                                <TabsContent value="skills">Skills</TabsContent>
+                                <TabsContent value="personal">
+                                    <div className="text-center xl:text-left">
+                                        <h3 className="h3 mb-4">Unmatched Service Quality for Over 10 years</h3>
+                                        <p className="subtitle max-w-xl mx-auto xl:mx-0">I specialized in crafting intuitive websites with cutting-edge technology, delivering dynamic and engaging user experiences.</p>
+                                        {/* Icons */}
+                                        <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                                            {infoData.map( (item, index) => {
+                                                return <div className="flex items-center gap-x-4 mx-auto xl:mx-0" key={index}>
+                                                    <div className="text-primary">{item.icon}</div>
+                                                    <div>{item.text}</div>
+                                                </div>
+                                            })}
+                                        </div>
+                                    </div>
+                                </TabsContent>
+                                <TabsContent value="qualifications">Qualifications Info</TabsContent>
+                                <TabsContent value="skills">Skills Info</TabsContent>
                             </div>
                         </Tabs>
                     </div>
