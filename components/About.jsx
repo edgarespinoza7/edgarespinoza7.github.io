c912af1eccf6f3999c74e6c394b07b26ba7d7cae
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-29 12:00:08 +0100

    27 -added skills data and style- OK

--- components/About.jsx
+++ components/About.jsx
@@ -240,8 +240,41 @@ const About = () => {
                 </TabsContent>
                 {/* Skills */}
                 <TabsContent value="skills">
-                  <div>
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">What I Use Everyday</h3>
+                    {/* Skills */}
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* Skill List */}
+                      <div>
+                        {getData(skillData, 'skills').data.map((item, index) => {
+                          const { name } = item;
+                          return (
+                            <div className="w-2/4 text-center xl:text-left mx-auto xl:mx-0" key={index}>
+                              <div className="font-medium">{name}</div>
+                            </div>
+                          )
 
+                        })}
+                      </div>
+                    </div>
+                    {/* Tools */}
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">Tools</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* Tool List */}
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, 'tools').data.map((item, index) => {
+                          const { imgPath } = item;
+                          return (
+                            <div key={index}>
+                              <Image src={imgPath} width={48} height={48} alt="" priority/>
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
                   </div>
                 </TabsContent>
               </div>
