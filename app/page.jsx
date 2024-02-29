8294ab729435c86caa9482ef7f9683ea5c1edb22
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-02-29 15:43:45 +0100

    28 -added Services Component and Style- OK

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Services from "@/components/ui/Services";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Services />
     </main>
   );
 }
