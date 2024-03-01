0f345f928f11255f487d731ecead4fd3b4d8ec42
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-03-01 13:33:08 +0100

    31 -finished work section- OK

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -6,24 +6,32 @@ import { Badge } from "./ui/badge";
 
 const ProjectCard = ({ project }) => {
 
-    const { category, image, name, description, link, github } = project;
+  const { category, image, name, description, link, github } = project;
 
-    return (
-        <Card className="group overflow-hidden ">
-            <CardHeader className="p-0">
-                {/* Image */}
-                <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
-                    <Image className="absolute bottom-0 shadow-2xl" src={image} width={247} height={250} alt="" priority/>
-                    {/* Buttons */}
-                </div>
-            </CardHeader>
-            <div className="h-full px-8 py-6">
-                <Badge className="uppercase text-sm font-medium mb-2 absolute top-4 left-5">{category}</Badge>
-                <h4 className="h4 mb-1">{name}</h4>
-                <p className="text-muted-foreground text-lg">{description}</p>
-            </div>
-        </Card>
-    )
+  return (
+    <Card className="group overflow-hidden ">
+      <CardHeader className="p-0">
+        {/* Image */}
+        <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
+          <Image className="absolute bottom-0 shadow-2xl" src={image} width={247} height={250} alt="" priority />
+          {/* Buttons Links*/}
+          <div className="flex gap-x-4">
+            <Link href={link} className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200">
+              <Link2Icon className="text-white" />
+            </Link>
+            <Link href={github} className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200">
+              <Github className="text-white" />
+            </Link>
+          </div>
+        </div>
+      </CardHeader>
+      <div className="h-full px-8 py-6">
+        <Badge className="uppercase text-sm font-medium mb-2 absolute top-4 left-5">{category}</Badge>
+        <h4 className="h4 mb-1">{name}</h4>
+        <p className="text-muted-foreground text-lg">{description}</p>
+      </div>
+    </Card>
+  )
 }
 
 export default ProjectCard;
