898b07e8f99714dd46adbb35b3495057b4d7fdd3
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-03-05 14:49:48 +0100

    34 -finished my projects page- OK

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -25,13 +25,13 @@ const ProjectCard = ({ project }) => {
           </div>
         </div>
       </CardHeader>
-      <div className="h-full px-8 py-6">
+      <div className="h-full px-8 py-6" >
         <Badge className="uppercase text-sm font-medium mb-2 absolute top-4 left-5">{category}</Badge>
         <h4 className="h4 mb-1">{name}</h4>
         <p className="text-muted-foreground text-lg">{description}</p>
       </div>
     </Card>
-  )
-}
+  );
+};
 
 export default ProjectCard;
